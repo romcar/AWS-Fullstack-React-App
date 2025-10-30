b29d1d033dfb0f5030f103c1aafb7013cf57a1c1
Erwin R. Carrasquilla <Rommel.carrasquilla@gmail.com>
2025-10-30 14:39:03 -0400

    Add Codacy config and update dependencies
    Added .codacy/codacy.yaml for code analysis configuration. Updated README with React + Vite template details and recommendations. Added @aws-amplify/ui-react and @types/aws-lambda to dependencies in package.json and package-lock.json. Updated Vite config to support new dependencies.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname),
+            "@src": path.resolve(__dirname, "src"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+        },
+    },
+});
