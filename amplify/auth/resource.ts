04d3281353f0f8b4b8beee94e605d5b1b99b0421
Erwin R. Carrasquilla <Rommel.carrasquilla@gmail.com>
2025-10-30 15:46:45 -0400

    Enhance auth config and add isDone to Todo model
    Updated auth resource to use detailed email and invitation configuration, and enabled preferredUsername attribute. Added an isDone boolean field with default value to the Todo model in the data resource. Also added a new README 2.md file describing the AWS full stack React app.

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,34 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+    loginWith: {
+        email: {
+            // NOTE: Instead of using a boolean to enable email sign-in, we explicitly
+            // set a configuration object to customize email settings. Such as the verification
+            // email that is sent to users when they sign up.
+            verificationEmailStyle: "CODE",
+            verificationEmailSubject:
+                "Use this code to verify your email address",
+            verificationEmailBody: (code) =>
+                `Your verification code is ${code()}`,
+            // NOTE: There are times where an admin might make a user account for them.
+            // In this case, we can customize the invitation email that is sent to users.
+            userInvitation: {
+                emailSubject: "Your temporary password",
+                emailBody: (username, password) =>
+                    `Ready to kick some ass?! Your username is ${username()} and your temporary password is ${password()}`,
+            },
+        },
+    },
+    // NOTE: Enable the ability to sign in with a username
+    userAttributes: {
+        preferredUsername: {
+            mutable: true,
+            required: false,
+        },
+    },
 });
